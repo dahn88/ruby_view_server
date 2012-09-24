0e4524a6e17ad85a2fecf63e9a8973cdcc35dba0
Daniel Ahn <ahnd88@gmail.com>
2012-09-23 17:27:41 -0700

    homework section 1 complete

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,17 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = 24
+template2 = ERB.new "The value of x + y is: <%= x+y %>"
+puts template2.result(binding)
+
+a = "boy"
+template3 = ERB.new "Hey there said the <%= a %>"
+puts template3.result(binding)
+
+dates = ['Mon', 'Tues', 'Wed']
+dates2 = ['Fri', 'Sat', 'Sun']
+template4 = ERB.new "I will be available on <%= dates %> and 
+will not be available <%= dates2 %>"
+puts template4.result(binding)
